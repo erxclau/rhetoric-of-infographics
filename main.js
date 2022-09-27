88e991a403b6c17d661623ac8e80b26337cacc52
erxclau <ericlau41802@gmail.com>
2022-09-27 04:13:49 -0400

    Create calendar

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
 import * as d3 from "d3";
 import csv from "./data/bereal.csv";
 
-window.onload = () => {};
+window.onload = () => {
+  const iso = d3.timeParse("%Y-%m-%d");
+  const data = csv.map((d) => ({ ...d, date: iso(d.date) }));
+
+  const graphic = d3.select("#graphic");
+
+  graphic
+    .selectAll("div")
+    .data(data)
+    .join("div")
+    .each(function (d) {
+      const parent = d3.select(this);
+      parent.attr("class", (d) => `mini parent ${d.activity}`);
+      const child = parent.append("div").attr("class", "child");
+
+      let hat = `var(--${d.hat ? "" : "not-"}wearing)`;
+      let mask = `var(--${d.mask ? "" : "not-"}wearing)`;
+
+      if (d.hat === null) {
+        hat = "transparent";
+      }
+
+      if (d.mask === null) {
+        mask = "transparent";
+      }
+
+      child.style(
+        "background",
+        `linear-gradient(to bottom, ${hat} 0, ${hat} 50%, ${mask} 50%, ${mask} 100%)`
+      );
+
+      if (d.activity === null) {
+        parent.style("border-style", "dashed");
+        child.style("border-style", "dashed");
+      }
+    });
+
+  graphic.select("div").style("grid-column", 5);
+};
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import dsv from "@rollup/plugin-dsv";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/rhetoric-of-infographics/",
-  plugins: [dsv()],
+  plugins: [
+    dsv({
+      processRow: (row) => {
+        Object.keys(row).forEach((key) => {
+          row[key] = row[key] === "false" ? false : row[key];
+          row[key] = row[key] === "true" ? true : row[key];
+          row[key] = row[key] === "null" ? null : row[key];
+        });
+      },
+    }),
+  ],
 });
